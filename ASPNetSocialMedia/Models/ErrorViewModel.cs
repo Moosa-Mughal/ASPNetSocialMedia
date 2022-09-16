0689195fe884bed246345d55830c1a1547e376e4
Helyxxx <tmuslehuddin@hotmail.com>
2022-09-15 20:45:22 -0400

    Add project files.

---
+++ ASPNetSocialMedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetSocialMedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

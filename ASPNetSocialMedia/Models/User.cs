a42818236c4c479dc6c0b2878a4ddda18db1ef23
Helyxxx <tmuslehuddin@hotmail.com>
2022-09-15 21:04:38 -0400

    Initial Migration

---
+++ ASPNetSocialMedia/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetSocialMedia.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{this.FirstName} {this.LastName}";
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+        public string? Biography { get; set; }
+        public string? ProfileImage { get; set; } = "NoImageFound.png";
+        public string Password { get; set; }
+        public string? Address { get; set; }
+        public int Age { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? AdminID { get; set; }
+    }
+}

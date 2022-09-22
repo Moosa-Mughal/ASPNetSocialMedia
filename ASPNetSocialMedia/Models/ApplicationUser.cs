2a0a1231fd6c98c2292575e06b36ab5c789e9633
JJredfield7 <JJredfield7@users.noreply.github.com>
2022-09-21 18:15:46 -0700

    Finally! Our biggest problem solved. Now every user will be tied to a single table using ASP.Net identity. Huge update, be sure to pull before making other changes.

---
+++ ASPNetSocialMedia/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetSocialMedia.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+        public string? Biography { get; set; }
+        public string? ProfileImage { get; set; } = "NoImageFound.png";
+        public string? Address { get; set; }
+        public int? Age { get; set; }
+    }
+}

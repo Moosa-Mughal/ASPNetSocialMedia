642d1a03607acd9e335a16a6de22ae9ff4b8e30c
masha4321 <masha4321@users.noreply.github.com>
2022-09-25 17:46:23 -0400

    i hate git

--- ASPNetSocialMedia/Data/ApplicationDbContext.cs
+++ ASPNetSocialMedia/Data/ApplicationDbContext.cs
@@ -12,10 +12,9 @@ namespace ASPNetSocialMedia.Data
         }
         public DbSet<ASPNetSocialMedia.Models.User> User { get; set; }
         public DbSet<ASPNetSocialMedia.Models.Friendship> Friendship { get; set; }
-<<<<<<< Updated upstream
-=======
-        public DbSet<ASPNetSocialMedia.Models.ApplicationUser> ApplicationUser { get; set; }
+
+        //public DbSet<ASPNetSocialMedia.Models.ApplicationUser> ApplicationUser { get; set; }
         public DbSet<ASPNetSocialMedia.Models.Post> Post { get; set; }
->>>>>>> Stashed changes
+
     }
 }

c274e1eb109835de7c1c5cc0a98e08e2650503cb
MajorMoose94 <MajorMoose94@users.noreply.github.com>
2022-09-27 15:30:34 -0400

    About us Page

--- ASPNetSocialMedia/Controllers/HomeController.cs
+++ ASPNetSocialMedia/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ASPNetSocialMedia.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
